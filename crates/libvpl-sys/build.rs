03114e6a29c012610f4cf9bdf9d8abfc349ecd47
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2026-04-14 13:54:24 -0500

    build: try to get windows build working

--- crates/libvpl-sys/build.rs
+++ crates/libvpl-sys/build.rs
@@ -1,9 +1,25 @@
 fn main() {
     let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
     if target_arch == "x86" || target_arch == "x86_64" {
-        pkg_config::Config::new()
-            .atleast_version("2.0")
-            .probe("vpl")
-            .expect("oneVPL (libvpl) not found. Please install the oneVPL-devel package.");
+        let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+
+        if target_os == "windows" {
+            #[cfg(target_os = "windows")]
+            {
+                if vcpkg::probe_package("vpl").is_ok() {
+                    return;
+                }
+            }
+
+            pkg_config::Config::new()
+                .atleast_version("2.0")
+                .probe("vpl")
+                .expect("oneVPL (vpl) not found.");
+        } else {
+            pkg_config::Config::new()
+                .atleast_version("2.0")
+                .probe("vpl")
+                .expect("oneVPL (libvpl) not found.");
+        }
     }
 }
