ebe1f4a326de27a796e7065e8ad9886b511a48f4
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2026-04-17 21:53:33 -0500

    feat: optimize order of software tonemap and scale

--- crates/ffpipeline/src/filter_chain.rs
+++ crates/ffpipeline/src/filter_chain.rs
@@ -208,6 +208,23 @@ impl FilterChain {
         self.filters = resolved;
     }
 
+    pub(crate) fn optimize(&mut self) {
+        // swap software scale before software tone map to reduce
+        // the amount of data that needs to be tone mapped
+        if let Some(tonemap_index) = self
+            .filters
+            .iter()
+            .position(|f| matches!(f, PipelineFilter::Video(VideoFilter::ToneMap { .. })))
+            && let Some(_) = self
+                .filters
+                .get(tonemap_index + 1)
+                .filter(|f| matches!(f, PipelineFilter::Video(VideoFilter::Scale { .. })))
+        {
+            log::debug!("swapping software scale filter before software tonemap filter");
+            self.filters.swap(tonemap_index, tonemap_index + 1);
+        }
+    }
+
     pub(crate) fn build(&mut self, audio_label: &str, video_label: &str) {
         self.audio_label = audio_label.to_owned();
         self.video_label = video_label.to_owned();
