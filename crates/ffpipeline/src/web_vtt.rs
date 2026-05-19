be17fecb1901d820757a12b59d06b7527bdc557d
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2026-05-19 10:47:26 -0500

    feedback

--- crates/ffpipeline/src/web_vtt.rs
+++ crates/ffpipeline/src/web_vtt.rs
@@ -25,7 +25,7 @@ pub async fn convert_to_vtt(
     let input_path = match &input.input_source {
         InputSource::Local(local) => Ok(local.path.clone()),
         InputSource::Http(http) => Ok(http.uri.clone()),
-        InputSource::Rtsp(rtsp) => Ok(rtsp.uri.clone()),
+        InputSource::Rtsp(_) => Err(FFPipelineError::FailedToConvertSubtitle),
         InputSource::Lavfi(_) => Err(FFPipelineError::FailedToConvertSubtitle),
     }?;
 
