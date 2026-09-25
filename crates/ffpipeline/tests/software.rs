5e74c7706f336cdea7fbf60f93b4b79efc4748fa
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2026-09-25 08:53:41 -0500

    fix: ffmpeg warning when using cuda and vulkan together

--- crates/ffpipeline/tests/software.rs
+++ crates/ffpipeline/tests/software.rs
@@ -158,6 +158,35 @@ async fn tonemap_hdr(
 #[rstest]
 #[tokio::test]
 #[ignore]
+async fn tonemap_hdr_watermark(
+    #[values("1080p_hevc_10_hdr.ts", "1080p_hevc_10_hdr_4x3.ts")] src: &'static str,
+    #[values("2560x1440", "1920x1080", "1280x720")] res: FrameSize,
+    #[values(("hevc", 8), ("hevc", 10))] vf: (&'static str, u8),
+    #[values("aac", "ac3")] af: AudioFormat,
+) {
+    let (vf_str, bpp) = vf;
+    if let Ok(vf) = VideoFormat::from_str(vf_str) {
+        run_software_test_case(TestCase {
+            fixture_name: src,
+            params: TestOutputParams {
+                audio_format: Some(af),
+                video_format: Some(vf),
+                video_size: Some(res),
+                bit_depth: Some(bpp),
+                watermark: Some(TestWatermark::default()),
+                ..TestOutputParams::default()
+            },
+            expected_video_codec: vf.to_string(),
+            expected_video_size: res,
+            expected_audio_codec: af.to_string(),
+        })
+        .await;
+    }
+}
+
+#[rstest]
+#[tokio::test]
+#[ignore]
 async fn tonemap_dv(
     #[values(
         "1080p_hevc_10_dv5.mp4",
